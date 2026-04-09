7e2957434e4060941cdf74390c5dfe35a9f77a42
Meet. Hirpara <meetkumarhirpara@gmail.com>
2026-04-09 22:55:34 +0530

    Update Protected.jsx

--- DAY 080 - Article App/Frontend/src/components/Protected.jsx
+++ DAY 080 - Article App/Frontend/src/components/Protected.jsx
@@ -7,10 +7,12 @@ const Protected = () => {
     const { user, loading } = useAuth();
 
     console.log(user);
+    console.log("Loading: ", loading);
 
     if (loading) return <p>Loading...</p>;
 
     if (!user) {
+        console.log("User not authorized");
         return <Navigate to="/signin" />
     }
 
